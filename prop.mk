9f4f4ef47814b7ad0a5c419fd15a7977bff8ed55
sooti <sooti85@gmail.com>
2019-06-26 23:50:51 +0200

    tissot: add discord to blacklist
    exposing aux to discord causes it to crash
    when making phone calls
    
    Change-Id: Ie3279a61cffadcbd09c06e3a07e0b6e22a331f45

--- prop.mk
+++ prop.mk
@@ -7,7 +7,7 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera \
+vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera,com.discord \
 vendor.camera.hal1.packagelist=com.whatsapp \
 persist.camera.dual.camera=0 \
 persist.camera.eis.enable=1
